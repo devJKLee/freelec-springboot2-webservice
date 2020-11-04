ef426fd3da0fa419dd81e4f2692fd199eadcb499
devJKLee <beneciajk@gmail.com>
2020-11-04 16:47:04 +0900

    1. 사용하지 않는 import 파일 정리

--- src/test/java/com/ai/school/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ai/school/springboot/web/HelloControllerTest.java
@@ -8,9 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
-
-import org.springframework.test.web.servlet.ResultActions;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
