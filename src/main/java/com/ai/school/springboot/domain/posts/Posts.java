b545d2b1b52eb09fcb7322e9507ab2df5d2f0e4f
devJKLee <beneciajk@gmail.com>
2020-11-04 17:56:26 +0900

    1. gradle 버전 업 2. jpa 관련 라이브러리 추가 3. jpa 관련 클래스 추가

---
+++ src/main/java/com/ai/school/springboot/domain/posts/Posts.java
@@ -0,0 +1,38 @@
+package com.ai.school.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Slf4j
+public class Posts {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
