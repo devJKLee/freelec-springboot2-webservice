d15fa8217e90c26ffc2211f2136698e8a9852fb1
devJKLee <beneciajk@gmail.com>
2020-11-16 14:29:55 +0900

    1. mustache 의존성 추가 2. 게시글 생성, 조회 view, controller, service 등 정의

--- src/main/java/com/ai/school/springboot/service/PostsService.java
+++ src/main/java/com/ai/school/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.ai.school.springboot.service;
 
 import com.ai.school.springboot.domain.posts.Posts;
 import com.ai.school.springboot.domain.posts.PostsRepository;
+import com.ai.school.springboot.web.dto.PostsListResponseDto;
 import com.ai.school.springboot.web.dto.PostsResponseDto;
 import com.ai.school.springboot.web.dto.PostsSaveRequestDto;
 import com.ai.school.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,10 +31,15 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
 
 }
