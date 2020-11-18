9e8d27d075f525c351f7e0734ee66f3348a3dd2e
devJKLee <beneciajk@gmail.com>
2020-11-18 17:24:41 +0900

    1. API 인증 / 인가 관련 소스 코드 추가 2. 기존 소스 수정

--- src/main/java/com/ai/school/springboot/domain/posts/Posts.java
+++ src/main/java/com/ai/school/springboot/domain/posts/Posts.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Slf4j
+@Where(clause = "is_deleted='false'")
 @Entity
 public class Posts extends BaseTimeEntity {
 
@@ -24,7 +26,11 @@ public class Posts extends BaseTimeEntity {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
+    @Column(nullable = false)
     private String author;
+
+    @Column(columnDefinition = "boolean default false", nullable = false)
+    private boolean is_deleted;
 
     @Builder
     public Posts(String title, String content, String author) {
@@ -36,6 +42,10 @@ public class Posts extends BaseTimeEntity {
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public void delete() {
+        this.is_deleted = true;
     }
 
 }
---
+++ src/main/java/com/ai/school/springboot/domain/user/User.java
@@ -0,0 +1,58 @@
+package com.ai.school.springboot.domain.user;
+
+import com.ai.school.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Where(clause = "is_deleted='false'")
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(columnDefinition = "boolean default false", nullable = false)
+    private boolean is_deleted;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public void delete() {
+        this.is_deleted = true;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}
