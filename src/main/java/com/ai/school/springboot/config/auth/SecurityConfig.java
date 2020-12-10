d55495ca0cb4d71c4e7da66301ffb2bc886d3696
devJKLee <beneciajk@gmail.com>
2020-12-10 10:47:29 +0900

    1. OAuth 2.0 설정 관련 사항 추가

--- src/main/java/com/ai/school/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/ai/school/springboot/config/auth/SecurityConfig.java
@@ -1,13 +1,13 @@
-package com.ai.school.springboot.config.auth;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
-    
-
-}
+//package com.ai.school.springboot.config.auth;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@RequiredArgsConstructor
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//
+//
+//
+//}
