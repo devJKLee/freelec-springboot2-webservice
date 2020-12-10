62bca785c725402af04f49cb8559b3adbfbb1d60
devJKLee <beneciajk@gmail.com>
2020-12-10 16:29:26 +0900

    1. 불필요한 주석 및 소스 제거 & 간격 수정 2. application.properties 내부의 datasource 부분을 독립적인 application 파일로 변경

--- src/main/java/com/ai/school/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/ai/school/springboot/config/auth/SecurityConfig.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -21,9 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                        .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
                 .and()
                     .logout()
                         .logoutSuccessUrl("/")
