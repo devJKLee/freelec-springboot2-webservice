eb2d5d831ba1fc5cb38094bca64bfac20e13b42d
devJKLee <beneciajk@gmail.com>
2020-12-10 11:28:27 +0900

    1. Spring Security Config / URL 별 설정값 추가
    2. OAuth 2.0 인증 관련 설정 내용 추가
    - 구글 로그인 서비스만 시범적으로 구현
    
    3. OAuth 인증 과정에서 사용될 DTO 구현

--- src/main/java/com/ai/school/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/ai/school/springboot/config/auth/SecurityConfig.java
@@ -1,13 +1,35 @@
-//package com.ai.school.springboot.config.auth;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@RequiredArgsConstructor
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//
-//
-//
-//}
+package com.ai.school.springboot.config.auth;
+
+import com.ai.school.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
